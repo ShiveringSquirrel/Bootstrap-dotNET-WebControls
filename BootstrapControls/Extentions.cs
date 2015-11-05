3de65f21fd86e2d35271a807b322f76eea1c2f0d
Joris <joris.onghena@uantwerpen.be>
2015-11-05 16:25:41 +0100

    Added try catch to GetPostBackControl

--- BootstrapControls/Extentions.cs
+++ BootstrapControls/Extentions.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.UI;
+﻿using System;
+using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace BootstrapControls
@@ -56,44 +57,50 @@ namespace BootstrapControls
                 return null;
 
             Control control = null;
-            // first we will check the "__EVENTTARGET" because if post back made by the controls
-            // which used "_doPostBack" function also available in Request.Form collection.
-            string controlName = page.Request.Params["__EVENTTARGET"];
-            if (!string.IsNullOrEmpty(controlName))
-            {
-                control = page.FindControl(controlName);
-            }
-            else
-            {
-                // if __EVENTTARGET is null, the control is a button type and we need to
-                // iterate over the form collection to find it
 
-                // ReSharper disable TooWideLocalVariableScope
-                string controlId;
-                Control foundControl;
-                // ReSharper restore TooWideLocalVariableScope
-
-                foreach (string ctl in page.Request.Form)
+            try
+            {
+                // first we will check the "__EVENTTARGET" because if post back made by the controls
+                // which used "_doPostBack" function also available in Request.Form collection.
+                string controlName = page.Request.Params["__EVENTTARGET"];
+                if (!string.IsNullOrEmpty(controlName))
                 {
-                    // handle ImageButton they having an additional "quasi-property" 
-                    // in their Id which identifies mouse x and y coordinates
-                    if (ctl.EndsWith(".x") || ctl.EndsWith(".y"))
-                    {
-                        controlId = ctl.Substring(0, ctl.Length - 2);
-                        foundControl = page.FindControl(controlId);
-                    }
-                    else
-                    {
-                        foundControl = page.FindControl(ctl);
-                    }
+                    control = page.FindControl(controlName);
+                }
+                else
+                {
+                    // if __EVENTTARGET is null, the control is a button type and we need to
+                    // iterate over the form collection to find it
 
-                    if (foundControl is System.Web.UI.WebControls.Button || foundControl is System.Web.UI.WebControls.ImageButton)
+                    // ReSharper disable TooWideLocalVariableScope
+                    string controlId;
+                    Control foundControl;
+                    // ReSharper restore TooWideLocalVariableScope
+
+                    foreach (string ctl in page.Request.Form)
                     {
-                        control = foundControl;
-                        break;
+                        // handle ImageButton they having an additional "quasi-property" 
+                        // in their Id which identifies mouse x and y coordinates
+                        if (ctl.EndsWith(".x") || ctl.EndsWith(".y"))
+                        {
+                            controlId = ctl.Substring(0, ctl.Length - 2);
+                            foundControl = page.FindControl(controlId);
+                        }
+                        else
+                        {
+                            foundControl = page.FindControl(ctl);
+                        }
+
+                        if (foundControl is System.Web.UI.WebControls.Button || foundControl is System.Web.UI.WebControls.ImageButton)
+                        {
+                            control = foundControl;
+                            break;
+                        }
                     }
                 }
             }
+            catch (Exception ex)
+            { }
 
             return control;
             //return control == null ? string.Empty : control.ID;
